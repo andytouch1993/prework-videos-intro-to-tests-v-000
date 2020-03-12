bee235abfcb3636eb6e8e1f5073eb6b91ebf70ec
Andy Touch <andytouch1993@icloud.com>
2020-03-12 23:04:27 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -6,8 +6,6 @@ describe "conversions" do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
     end
-  end
-  describe '#ounces_to_grams' do
     it 'given 1, returns 28.3495' do
       grams = ounces_to_grams(1)
       expect(grams).to eq(28.3495)
